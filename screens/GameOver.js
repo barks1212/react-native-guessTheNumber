11de31881f7b92acdf4865fc926c7ae84b9f0cae
barks1212 <willbarkerwork@gmail.com>
2019-10-24 15:33:04 +0100

    restart game handler

--- screens/GameOver.js
+++ screens/GameOver.js
@@ -1,11 +1,14 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
 import GameScreen from "./GameScreen";
 
 const GameOver = props => {
   return (
     <View style={styles.screen}>
       <Text>The Game is Over!</Text>
+      <Text>Number of rounds: {props.roundsNumber}</Text>
+      <Text>Number was: {props.userNumber}</Text>
+      <Button title="NEW GAME" onPress={props.onRestart} />
     </View>
   );
 };
