7c072402475a6f5f270a3534e51d747986efa386
barks1212 <willbarkerwork@gmail.com>
2019-10-25 08:18:28 +0100

    dimensions api

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -6,7 +6,8 @@ import {
   Button,
   TouchableWithoutFeedback,
   Keyboard,
-  Alert
+  Alert,
+  Dimensions
 } from "react-native";
 import Card from "../components/Card";
 import Input from "../components/Input";
@@ -126,7 +127,8 @@ const styles = StyleSheet.create({
     paddingHorizontal: 15
   },
   button: {
-    width: 100
+    // width: 100
+    width: Dimensions.get("window").width / 4
   },
   input: {
     width: 50,
